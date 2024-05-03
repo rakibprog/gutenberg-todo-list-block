754b109200383b22ba035f40e1b4f32ba906be07
rakibprog <contact.engrakib@gmail.com>
2024-05-03 18:20:59 +0600

    todo list init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd39d2ca984116c79b76c');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4a17d0fe30d2199eca62');
